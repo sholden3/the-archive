94fdc04eba20a0fe6b2ad1d0f9c17daa39c6eb79
Wes Holden <wesholden1992@gmail.com>
2022-01-27 13:06:59 -0500

    Set up our main-content component, as well as adding the initial logic for receiving the boolean for if our size is small or not.

--- the-archive/src/app/content-wrapper/main-content/main-content.component.ts
+++ the-archive/src/app/content-wrapper/main-content/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-content',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainContentComponent implements OnInit {
 
-  constructor() { }
+  @Input() isScreenSmall: boolean | null;
+
+  constructor() { 
+    this.isScreenSmall = null;
+  }
 
   ngOnInit(): void {
   }
