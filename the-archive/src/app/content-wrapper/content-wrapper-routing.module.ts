c3d6de371d8ffd5038386a5699e478ca128f65f4
Wes Holden <wesholden1992@gmail.com>
2022-01-27 11:22:04 -0500

    Fixed innapropriate use of forRoot to forChild. Added code for testing for side-nav.component

--- the-archive/src/app/content-wrapper/content-wrapper-routing.module.ts
+++ the-archive/src/app/content-wrapper/content-wrapper-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forChild(routes)
   ]
 })
 export class ContentWrapperRoutingModule { }
--- the-archive/src/app/content-wrapper/side-nav/side-nav.component.ts
+++ the-archive/src/app/content-wrapper/side-nav/side-nav.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'app-side-nav',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
-  constructor() { }
+  public isScreenSmall: boolean | null;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) { 
+    this.isScreenSmall = null;
+    this.sidenav = null;
+  }
+
+    @ViewChild(MatSidenav) sidenav: MatSidenav | null;
 
   ngOnInit(): void {
+    this.breakpointObserver
+      .observe([
+        `(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`
+      ])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      });
+
+    this.router.events.subscribe(() => {
+      if (this.isScreenSmall) {
+        this.sidenav!.close();
+      }
+    })
   }
 
 }
